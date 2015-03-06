b224ad683c8e22b90da14a227e6e0698cd999980
Kartavtsev Sergey <crazyserega1994@mail.ru>
2015-03-06 15:42:13 +0200

    Added comments to @html decorator

--- decorators.py
+++ decorators.py
@@ -1,4 +1,15 @@
 def html(file_name):
+	"""Loads and outputs file directrly to response stream
+
+	Args:
+		file_name (str): File's filename to read
+
+	Returns:
+		func: Decorated function
+
+	Raises:
+		IOError: If file does not exist
+	"""
     def html_decorator(func):
         def func_wrapper(*args, **kwargs):
             self = args[0]
